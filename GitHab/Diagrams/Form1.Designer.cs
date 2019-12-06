d4d25fb43a44b4df3cf9553f4baf873bef478f90
klim86klim <klim86kr@gmail.com>
2019-12-07 01:52:10 +0200

    edit done

--- GitHab/Diagrams/Form1.Designer.cs
+++ GitHab/Diagrams/Form1.Designer.cs
@@ -28,34 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton_Rect = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Line = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Ellips = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Figure = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Color = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // toolStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(405, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton_Rect,
+            this.toolStripButton_Line,
+            this.toolStripButton_Ellips,
+            this.toolStripButton_Figure,
+            this.toolStripButton1,
+            this.toolStripButton_Color});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton_Rect
+            // 
+            this.toolStripButton_Rect.CheckOnClick = true;
+            this.toolStripButton_Rect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_Rect.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Rect.Image")));
+            this.toolStripButton_Rect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Rect.Name = "toolStripButton_Rect";
+            this.toolStripButton_Rect.Size = new System.Drawing.Size(34, 22);
+            this.toolStripButton_Rect.Text = "Rect";
+            this.toolStripButton_Rect.Click += new System.EventHandler(this.toolStripButton_Rect_Click);
+            // 
+            // toolStripButton_Line
+            // 
+            this.toolStripButton_Line.CheckOnClick = true;
+            this.toolStripButton_Line.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_Line.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Line.Image")));
+            this.toolStripButton_Line.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Line.Name = "toolStripButton_Line";
+            this.toolStripButton_Line.Size = new System.Drawing.Size(33, 22);
+            this.toolStripButton_Line.Text = "Line";
+            this.toolStripButton_Line.Click += new System.EventHandler(this.toolStripButton_Line_Click);
+            // 
+            // toolStripButton_Ellips
+            // 
+            this.toolStripButton_Ellips.CheckOnClick = true;
+            this.toolStripButton_Ellips.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_Ellips.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Ellips.Image")));
+            this.toolStripButton_Ellips.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Ellips.Name = "toolStripButton_Ellips";
+            this.toolStripButton_Ellips.Size = new System.Drawing.Size(38, 22);
+            this.toolStripButton_Ellips.Text = "Ellips";
+            this.toolStripButton_Ellips.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton_Figure
+            // 
+            this.toolStripButton_Figure.CheckOnClick = true;
+            this.toolStripButton_Figure.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_Figure.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Figure.Image")));
+            this.toolStripButton_Figure.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Figure.Name = "toolStripButton_Figure";
+            this.toolStripButton_Figure.Size = new System.Drawing.Size(49, 22);
+            this.toolStripButton_Figure.Text = "FigLine";
+            this.toolStripButton_Figure.Click += new System.EventHandler(this.toolStripButton_Figure_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(784, 436);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.toolStripButton_Figure_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Clear";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click_1);
+            // 
+            // toolStripButton_Color
+            // 
+            this.toolStripButton_Color.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_Color.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_Color.Image")));
+            this.toolStripButton_Color.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Color.Name = "toolStripButton_Color";
+            this.toolStripButton_Color.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_Color.Text = "toolStripButton2";
+            this.toolStripButton_Color.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 407);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Rect;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Line;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Ellips;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Figure;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Color;
     }
 }
 
--- GitHab/Diagrams/Form1.cs
+++ GitHab/Diagrams/Form1.cs
@@ -12,14 +12,163 @@ namespace Diagrams
 {
     public partial class Form1 : Form
     {
+        //Color CurrentColor = Color.Black;
+        Point pointStat;
+        Point pointEnd;
+        List<int> listP = new List<int>();
+        Graphics g;
+        Bitmap bmp;
+       
+       
         public Form1()
         {
             InitializeComponent();
-        }
 
+        }
+        private void LoadBackGroundImage()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Image im = Image.FromFile("background.png");
+            bmp = new Bitmap(im, pictureBox1.Size.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("!!!!");
+            LoadBackGroundImage();
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+            ToolStripButton stButton = toolStrip1.Items.OfType<ToolStripButton>().FirstOrDefault(t => t.Checked);
+            if (stButton != null)
+            {
+                // rectangle config vars
+                int minX = pointStat.X < e.X ? pointStat.X : e.X;
+                int minY = pointStat.Y < e.Y ? pointStat.Y : e.Y;
+                int width = Math.Abs(pointStat.X - e.X);
+                int height = Math.Abs(pointStat.Y - e.Y);
+
+                if (stButton.Text == "Rect")
+                {
+                    Pen stRectangle = new Pen(Color.Red, 2);
+                    g.DrawRectangle(stRectangle, new Rectangle(new Point(minX, minY), new Size(width, height)));
+                }
+                else if (stButton.Text == "Ellips")
+                {
+                    Pen stEllips = new Pen(Color.Magenta, 2);
+                    g.DrawEllipse(stEllips, new Rectangle(new Point(minX, minY), new Size(width, height)));
+                }
+                else if (stButton.Text == "Line")
+                {
+                    Pen stLine = new Pen(Color.Brown, 2);
+                    g.DrawLine(stLine, pointStat, e.Location);
+                }
+                pictureBox1.Image = bmp;
+            }
+           
+        }
+
+      
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            ToolStripButton stButton = toolStrip1.Items.OfType<ToolStripButton>().FirstOrDefault(t => t.Checked);
+           
+            if (stButton != null)
+            {
+                //Pen
+                if (stButton.Text == "FigLine")
+                {
+                    SolidBrush s = new SolidBrush(Color.Black);
+                    g.FillEllipse(Brushes.Black, e.X, e.Y,4, 4);
+                   
+                }
+            }
+            
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            pointStat = e.Location;
+        }
+
+        private void toolStripButton_Rect_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton_Rect.Checked)
+            {
+                toolStripButton_Rect.Checked = true;
+                toolStripButton_Line.Checked = false;
+                toolStripButton_Ellips.Checked = false;
+                toolStripButton_Figure.Checked = false;
+
+            }
+        }
+
+        private void toolStripButton_Line_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton_Line.Checked)
+            {
+                toolStripButton_Rect.Checked = false;
+                toolStripButton_Line.Checked = true;
+                toolStripButton_Ellips.Checked = false;
+                toolStripButton_Figure.Checked = false;
+            }
+        }
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton_Ellips.Checked)
+            {
+                toolStripButton_Rect.Checked = false;
+                toolStripButton_Line.Checked = false;
+                toolStripButton_Ellips.Checked = true;
+                toolStripButton_Figure.Checked = false;
+            }
+        }
+
+      
+      
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void toolStripButton_Figure_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton_Figure.Checked)
+            {
+                toolStripButton_Figure.Checked = true;
+                toolStripButton_Rect.Checked = false;
+                toolStripButton_Line.Checked = false;
+                toolStripButton_Ellips.Checked = false;
+               
+            }
+        }
+       
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            g.Clear(Color.Teal);
+            pictureBox1.Image = bmp;
+            LoadBackGroundImage();
+            pictureBox1.Invalidate();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog()==DialogResult.OK)
+            {
+                toolStripButton_Color.BackColor = color.Color;
+
+            }
         }
     }
 }
