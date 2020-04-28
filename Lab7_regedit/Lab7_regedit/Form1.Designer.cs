274eb718f2c0aea97aa423bb929e59b238a4189a
klim86klim <49413026+klim86klim@users.noreply.github.com>
2020-04-28 23:52:04 +0300

    Использование реестра
    Написать программу, которая отображает реестр Windows в TreeView.
    При выборе узла, должны отображаться в ListView значения.

---
+++ Lab7_regedit/Lab7_regedit/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Lab7_regedit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 19);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(306, 584);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(743, 614);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Regedit";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(318, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(405, 584);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 629);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 656);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab7_regedit/Lab7_regedit/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace Lab7_regedit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("Имя",150);
+            listView1.Columns.Add("Тип", 150);
+            listView1.Columns.Add("Значение", 150);
+            listView1.View = View.Details;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<RegistryKey> rootKeys = new List<RegistryKey>();
+            rootKeys.Add(Registry.ClassesRoot);
+            rootKeys.Add(Registry.CurrentUser);
+            rootKeys.Add(Registry.LocalMachine);
+            rootKeys.Add(Registry.Users);
+            rootKeys.Add(Registry.CurrentConfig);
+
+            foreach (RegistryKey key in rootKeys)
+            {
+                TreeNode treeNode = new TreeNode(key.Name);
+                treeNode.Tag = key;
+                FillNodes(treeNode, 0);
+                treeView1.Nodes.Add(treeNode);
+
+            }
+        }
+
+        private void FillNodes(TreeNode root, int depth)
+        {
+            if (depth > 5)
+                return;
+            
+            RegistryKey key = root.Tag as RegistryKey;
+            foreach (string nodes in key.GetSubKeyNames())
+            {
+                try
+                {
+                    RegistryKey subKey = key.OpenSubKey(nodes);
+                    TreeNode childNodes = new TreeNode(nodes);
+                    if (childNodes.Nodes.Count<0)
+                    {
+                        throw new Exception("Error to add childNodes");
+                    }
+                                       
+                    childNodes.Tag = subKey;
+                    FillNodes(childNodes, ++depth);
+                    root.Nodes.Add(childNodes);
+
+                }
+               catch(Exception)
+                {
+                    
+                }
+            }
+
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            RegistryKey selecKey = e.Node.Tag as RegistryKey;
+            listView1.Items.Clear();
+            foreach (string str in selecKey.GetValueNames())
+            {
+                try
+                {                       
+                   ListViewItem item = listView1.Items.Add(str);
+                   item.SubItems.Add(selecKey.GetValue(str).ToString());
+                   item.SubItems.Add(selecKey.GetValueKind(str).ToString());
+                   label1.Text = e.Node.FullPath.ToString();
+                }
+                catch (Exception)
+                {
+                }
+            }
+            
+          
+        }
+
+    }
+}
